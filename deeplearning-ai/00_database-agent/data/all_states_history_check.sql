be3080d2483150085bd49c385639d4bbca5b384d
jmoianes <jmoianes@gmail.com>
2025-08-21 15:22:28 +0200

    SQL Agent

---
+++ deeplearning-ai/00_database-agent/data/all_states_history_check.sql
@@ -0,0 +1,3 @@
+SELECT SUM(hospitalizedIncrease) FROM all_states_history WHERE state = 'NY' AND date LIKE '2020-10%';
+SELECT SUM(hospitalizedIncrease) FROM all_states_history WHERE date LIKE '2020-10%';
+
